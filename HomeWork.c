abf3253454eb40de717f2880ee97ed26303c2b5b
Ali <aliareya2003@gmail.com>
2024-06-22 20:54:03 +0430

    first commite

---
+++ HomeWork.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+  // first test
+  for(int i =100 ; i>=0; i--){
+     printf("%d\t" , i);
+  }
+
+   printf("\n");
+
+  //secound test 
+
+  for(int x =6; x<=66 ;x+=6){
+     printf("%d \t", x);
+  }
+
+   printf("\n");
+
+  //three test
+
+  for(int y=30 ; y>=3; y-=3){
+   printf("%d \t", y);
+  }
+
+   printf("\n \t");
+  //four test
+
+  for(int a =2 ;a<=17 ;a+=3){
+     printf("%d \t" ,a);
+  }
+
+   printf("\n");
+  //five test 
+
+  for(int e =44; e>=0 ;e-=11){
+     printf("%d \t",e);
+  }
+
+}
